40e2733248ee15e05bafb94945134b1f35148e2a
fauzan010 <158558285+fauzan010@users.noreply.github.com>
2025-12-07 19:28:20 +0700

    Delete myapi directory

--- myapi/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-Type: application/json");
-
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    echo json_encode(["status" => "error", "message" => "Harus POST"]);
-    exit;
-}
-
-echo json_encode(["status" => "success", "message" => "Berhasil"]);
